ad1b00ac0e50d414c7f4d5d1b34f81843da610a0
PainJ <painjsimpson@gmail.com>
2019-01-30 19:41:50 -0700

    first component test

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,3 @@
 // frontend/packs/application.js
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works, yo!");
+import "init";
+import "components/page/page";
